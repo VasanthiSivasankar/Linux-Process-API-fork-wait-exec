cf664b1175b1d7aa0ff9aaf42104cb3098b6fa53
VasanthiSivasankar <161431945+VasanthiSivasankar@users.noreply.github.com>
2024-05-08 20:21:05 +0530

    Update execcheck2.c

--- execcheck2.c
+++ execcheck2.c
@@ -1,1 +1,41 @@
 //C Program to execute Linux system commands using Linux API system calls exec() family
+//C Program to execute Linux system commands using Linux API system calls exec() family
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+int main()
+{
+    int status;
+    printf("Running ps with execlp\n");
+    execl("ps","ps","ax",NULL);
+    wait(&status);
+    if (WIFEXITED(status))
+        printf("child exited with the status of %d\n",WEXITSTATUS(status));
+    else    
+        puts("child did not exit succesfully\n");
+    printf("Done.\n");
+    exit(0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
